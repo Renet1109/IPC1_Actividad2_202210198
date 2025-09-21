906617706441147bc05dac70d6eb11533f07ff9c
Renet1109 <107593825+Renet1109@users.noreply.github.com>
2025-09-20 23:54:19 -0600

    Add files via upload

---
+++ Practica2_202210198/src/practica2_202210198/Practica2_202210198.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica2_202210198;
+
+/**
+ *
+ * @author willi
+ */
+public class Practica2_202210198 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
